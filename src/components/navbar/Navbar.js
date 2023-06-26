c7a267e65f6c255a0020c334590e92117a723af6
sjcode99 <misuraj2611@gmail.com>
2023-06-26 15:16:56 +0530

    css for sidebar finished

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,12 +10,13 @@ const Navbar = () => {
   return (
     <div className="h-20 flex items-center justify-center text-gray-500">
       <div className="w-full p-4 flex items-center justify-between">
-        <div className="hidden md:flex items-center rounded-md bg-gray-100 px-4 py-2 ml-8">
+        <div className="w-[417px] h-[44px] hidden md:flex items-center rounded-md bg-gray-100 px-4 py-2 ml-8">
           <img src={searchbar} alt="" />
           <input
             type="text"
             placeholder="Search for anything..."
-            className="w-[471px] h-[11px] hidden md:block outline-none bg-gray-100 p-3 ml-3"
+            className="hidden md:block outline-none bg-gray-100 p-3 ml-3"
+            // className="w-[471px] h-[11px] hidden md:block outline-none bg-gray-100 p-3 ml-3"
           />
         </div>
         <div className="flex items-center justify-center">
--- src/components/sideBarItem/SidebarDott.js
+++ src/components/sideBarItem/SidebarDott.js
@@ -1,16 +1,19 @@
-
 const SidebarDot = ({ text, color }) => {
   const bgColor = `w-[8px] h-[8px] rounded-full ${color}`;
 
   return (
     <li
-      className={`flex items-center justify-between mx-3 pl-3 py-[10px] rounded-md ${
+      className={`flex items-center justify-between mx-3 pl-3 py-[10px] rounded-md font-medium ${
         text === "Mobile App" && "font-semibold bg-purple-200 bg-opacity-30"
       } hover:cursor-pointer transition relative`}
     >
       <div className="flex items-center justify-center">
         <div className={bgColor}></div>
-        <span className="text-base ml-3">{text}</span>
+        {text === "Mobile App" ? (
+          <span className="text-base ml-3">{text}</span>
+        ) : (
+          <span className="text-base ml-3 text-[#787486]">{text}</span>
+        )}
       </div>
       <div
         className={`flex justify-center items-center absolute top-1/2 right-2 mr-4 transform -translate-y-1/2 ${
@@ -24,6 +27,5 @@ const SidebarDot = ({ text, color }) => {
     </li>
   );
 };
-
 
 export default SidebarDot;
